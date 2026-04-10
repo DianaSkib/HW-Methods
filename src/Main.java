b0ccc13e4e826d90995e589eb82ff5e1249219f5
Диана <ashkhar.ik@mail.ru>
2026-04-10 10:36:54 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-
-        /// task 1
+        // task 1
         int year = 2021;
         printLeapYearResult(year);
+
+        // task 2
+        recommendAppVersion(0, 2014); //   task 2
+        recommendAppVersion(1, 2016); //   task 2
+        recommendAppVersion(0, 2030); //   task 2
+        recommendAppVersion(2, 2020); //   task 2
     }
 
     public static void printLeapYearResult(int year) {
@@ -18,5 +23,31 @@ public class Main {
 
     public static boolean isLeapYear(int year) {
         return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    // task 2
+    public static void recommendAppVersion(int osType, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        boolean isLegacyDevice = clientDeviceYear < 2015 || clientDeviceYear > currentYear;
+
+        String osName;
+        String message;
+
+        if (osType == 0) {
+            osName = "iOS";
+        } else if (osType == 1) {
+            osName = "Android";
+        } else {
+            System.out.println("Неподдерживаемая операционная система");
+            return;
+        }
+
+        if (isLegacyDevice) {
+            message = "Установите облегчённую версию приложения для " + osName + " по ссылке";
+        } else {
+            message = "Установите приложение для " + osName + " по ссылке";
+        }
+
+        System.out.println(message);
     }
 }
